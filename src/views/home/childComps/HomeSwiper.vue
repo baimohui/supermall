f4a590a3ff8366268185c9d08301a6714b71e7d3
baimohui <3146216206@qq.com>
2020-11-17 16:37:44 +0800

    renew0

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <swiper>
+      <swiper-item v-for="(item, key) in banners" :key="key">
+        <!-- href和item前要加： -->
+        <a :href="item.link">
+          <img :src="item.image" alt="" />
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper/index.js";
+export default {
+  name: "HomeSwiper",
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+};
+</script>
